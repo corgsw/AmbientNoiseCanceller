b9404527d10dea95d4a6b1020e6ef3111af49abe
MattyBechtel <mbechtel@qis.net>
2020-04-16 02:28:01 -0400

    First patch. Currently the program takes input from the microphone and parrots it back over the speakers, with a full right pan.

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
 
 public class Main {
 	public static void main (String [] args) {
-		System_frame sys = new System_frame();
-		sys.audio_frame(); //initialize default frame here
+		/*System_frame sys = new System_frame();
+		sys.audio_frame(); //initialize default frame here*/
+		SoundOut.soundtest();
 	}
 }
