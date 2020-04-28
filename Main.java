cf3453f26b89aa3d3c9bb296a25c01ff4c813e81
MattyBechtel <mbechtel@qis.net>
2020-04-27 23:13:43 -0400

    Third patch. Added some QOL improvements with accessor functions for easier communication with the UI/Frontend.

--- Main.java
+++ Main.java
@@ -3,6 +3,6 @@ public class Main {
 	public static void main (String [] args) {
 		/*System_frame sys = new System_frame();
 		sys.audio_frame(); //initialize default frame here*/
-		SoundOut.soundtest();
+		SoundOut.StartSoundOut();
 	}
 }
--- SoundOut.java
+++ SoundOut.java
@@ -1,10 +1,10 @@
 import javax.sound.sampled.*;
-import javax.xml.transform.Source;
 import java.io.ByteArrayOutputStream;
-import java.util.Arrays;
 
 public class SoundOut {
-    static AudioFormat format = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 44100, 16, 2, 4, 44100, true);
+    static boolean exit=false;
+    static float pan=0;
+    static AudioFormat format = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 44100, 16, 2, 4, 44100, false);
     public static void soundtest() {
         try {
             DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
@@ -20,23 +20,24 @@ public class SoundOut {
             Thread monitorThread = new Thread() {
                 @Override
                 public void run() {
-                    target.start();
-                    source.start();
-                    int readBytes;
-                    short inverse;
-                    byte[] data = new byte[target.getBufferSize()];
-                    FloatControl pan = (FloatControl) source.getControl(FloatControl.Type.PAN);
-                    pan.setValue(0);
-                    while (true) {
-                        readBytes = target.read(data, 0, data.length);
-                        for(int i = 0; i < readBytes; i+=2)
-                        {
-                            inverse = (short) -((short)(data[i+1] << 8 | data[i] & 0xff));
-                            System.out.println(inverse/100);
-                            data[i] = (byte) (inverse & 0xff);
-                            data[i+1] = (byte) ((inverse >> 8) & 0xff);
+                    while (!exit) {
+                        target.start();
+                        source.start();
+                        int readBytes;
+                        short inverse;
+                        byte[] data = new byte[target.getBufferSize()];
+                        FloatControl panorama = (FloatControl) source.getControl(FloatControl.Type.PAN);
+                        panorama.setValue(pan);
+                        while (true) {
+                            readBytes = target.read(data, 0, data.length);
+                            for (int i = 0; i < readBytes; i += 2) {
+                                inverse = (short) -((short) (data[i + 1] << 8 | data[i] & 0xff));
+                                System.out.println(inverse / 100);
+                                data[i] = (byte) (inverse & 0xff);
+                                data[i + 1] = (byte) ((inverse >> 8) & 0xff);
+                            }
+                            source.write(data, 0, readBytes);
                         }
-                        source.write(data, 0, readBytes);
                     }
                 }
             };
@@ -50,6 +51,17 @@ public class SoundOut {
         } catch (LineUnavailableException lue) {
             lue.printStackTrace();
         }
+    }
+
+    public static void StopSoundOut(){
+        exit=true;
+    }
+    public static void StartSoundOut(){
+        exit=false;
+        soundtest();
+    }
+    public void setPan(int p){
+        pan=p;
     }
 
 }
